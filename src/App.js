a25c20dc3059036a07de65087d1bb826f85782da
Amelia Sun <amelia.sun905@gmail.com>
2024-04-24 23:45:29 -0700

    yearly update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,81 @@
-import React, { useState } from "react";
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
+import React, { useState } from 'react'
+import NewExpense from './components/NewExpense/NewExpense'
+import Expenses from './components/Expenses/Expenses'
 
 const DUMMY_DATA = [
   {
-    id: "e1",
-    title: "Toilet Paper",
+    id: 'e1',
+    title: 'Toilet Paper',
     amount: 94.12,
     date: new Date(2022, 2, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: "e3",
-    title: "Car Insurance",
+    id: 'e3',
+    title: 'Car Insurance',
     amount: 294.67,
     date: new Date(2022, 2, 28),
   },
   {
-    id: "e4",
-    title: "New Desk (Wooden)",
+    id: 'e4',
+    title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
   },
   {
-    id: "e5",
-    title: "New Desk (White)",
+    id: 'e5',
+    title: 'New Desk (White)',
     amount: 450,
     date: new Date(2021, 5, 12),
   },
   {
-    id: "e6",
-    title: "Water",
+    id: 'e6',
+    title: 'Water',
     amount: 2,
     date: new Date(2020, 2, 14),
   },
   {
-    id: "e7",
-    title: "Notebook",
+    id: 'e7',
+    title: 'Notebook',
     amount: 5,
     date: new Date(2023, 1, 14),
   },
   {
-    id: "e8",
-    title: "Plants",
+    id: 'e8',
+    title: 'Plants',
     amount: 20,
     date: new Date(2023, 0, 14),
   },
-];
+  {
+    id: 'e9',
+    title: 'Dental',
+    amount: 400,
+    date: new Date(2024, 1, 27),
+  },
+  {
+    id: 'e10',
+    title: 'Dance Classes',
+    amount: 200,
+    date: new Date(2024, 3, 14),
+  },
+]
 
 // arrow functions
 const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_DATA);
+  const [expenses, setExpenses] = useState(DUMMY_DATA)
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpense) => {
-      return [expense, ...prevExpense];
-    });
-  };
+      return [expense, ...prevExpense]
+    })
+  }
 
   return (
-    <div className="APP_js level_one parent">
+    <div className='APP_js level_one parent'>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
-  );
-}; // end of the function
+  )
+} // end of the function
 
 // 1. export the file
-export default App;
+export default App
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import ExpensesList from "./ExpensesList";
-import ExpensesChart from "./ExpensesChart";
-import "./Expenses.css";
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
+import './Expenses.css'
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2023");
+  const [filteredYear, setFilteredYear] = useState('2024')
 
   const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+    setFilteredYear(selectedYear)
+  }
 
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+    return expense.date.getFullYear().toString() === filteredYear
+  })
 
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
@@ -28,7 +28,7 @@ const Expenses = (props) => {
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default Expenses;
+export default Expenses
